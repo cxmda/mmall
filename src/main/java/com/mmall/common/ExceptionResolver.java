78cc28c2bad1b366b46dd5d64f62ce865f411d86
Chen Qiang <chenqiang@gmail.com>
2020-07-29 17:26:02 +0800

    "使用SpringMVC全局异常"

---
+++ src/main/java/com/mmall/common/ExceptionResolver.java
@@ -0,0 +1,29 @@
+package com.mmall.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author chenqiang
+ * @create 2020-07-29 11:52
+ */
+@Slf4j
+@Component
+public class ExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        log.error("{} Exception",httpServletRequest.getRequestURI(),e);
+        //当使用是jackson2.x的时候使用MappingJackson2JsonView，目前使用的是1.9。
+        ModelAndView modelAndView = new ModelAndView(new MappingJacksonJsonView());
+        modelAndView.addObject("status",ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg","接口异常,详情请查看服务端日志的异常信息");
+        modelAndView.addObject("data",e.toString());
+        return modelAndView;
+    }
+}
