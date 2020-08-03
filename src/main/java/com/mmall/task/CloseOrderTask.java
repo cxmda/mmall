732886b9356c96adb4dfde00d561c1816764b823
Chen Qiang <chenqiang@gmail.com>
2020-08-03 16:30:01 +0800

    "使用Redis实现分布式锁"

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -1,11 +1,16 @@
 package com.mmall.task;
 
+import com.mmall.common.Const;
 import com.mmall.service.IOrderService;
 import com.mmall.util.PropertiesUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 
 /**
  * @author chenqiang
@@ -18,11 +23,73 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
-    @Scheduled(cron = "* */1 * * * ?")//每1分钟(每个1分钟的整数倍)
-    public void closeOrderTask(){
+    @PreDestroy
+    public void delLock() {
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+    }
+
+    //@Scheduled(cron = "* */1 * * * ?")//每1分钟(每个1分钟的整数倍)
+    public void closeOrderTaskV1() {
         log.info("关闭订单定时任务开始");
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
         iOrderService.closeOrder(hour);
         log.info("关闭订单定时任务结束");
+    }
+
+    //@Scheduled(cron = "* */1 * * * ?")
+    public void closeOrderTaskV2() {
+        log.info("关闭订单定时任务开始");
+        long lockTime = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
+
+        Long setNxResult = RedisShardedPoolUtil.setNx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTime));
+        if (setNxResult != null && setNxResult.intValue() == 1) {
+            //如果返回值是1，代表设置成功，获取锁
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        } else {
+            log.info("没有获取到分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }
+        log.info("关闭订单定时任务结束");
+    }
+
+    @Scheduled(cron = "* */1 * * * ?")
+    public void closeOrderTaskV3() {
+        log.info("关闭订单定时任务开始");
+        long lockTime = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
+
+        Long setNxResult = RedisShardedPoolUtil.setNx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTime));
+        if (setNxResult != null && setNxResult.intValue() == 1) {
+            //如果返回值是1，代表设置成功，获取锁
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        } else {
+            //未获取到锁，继续判断，判断时间戳，看是否可以重置并获取到锁
+            String lockValueStr = RedisShardedPoolUtil.get(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            if (lockValueStr != null && System.currentTimeMillis() > Long.parseLong(lockValueStr)) {
+                String getSetResult = RedisShardedPoolUtil.getSet(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf((System.currentTimeMillis() + lockTime)));
+                //再次用当前时间戳getset。
+                //返回给定的key的旧值，->旧值判断，是否可以获取锁
+                //当key没有旧值时，即key不存在时，返回nil ->获取锁
+                //这里我们set了一个新的value值，获取旧的值。
+                if (getSetResult == null || (getSetResult != null && StringUtils.equals(lockValueStr,getSetResult))) {
+                    //真正获取到锁
+                    closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                } else {
+                    log.info("没有获取到分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                }
+            } else {
+                log.info("没有获取到分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            }
+            log.info("没有获取到分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }
+        log.info("关闭订单定时任务结束");
+    }
+
+    public void closeOrder(String lockName) {
+        //设置lockName的有效期，防止死锁
+        RedisShardedPoolUtil.expire(lockName, 5);
+        log.info("获取{}，ThreadName:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
+        iOrderService.closeOrder(hour);
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        log.info("释放：{}，ThreadName:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
     }
 }
--- src/main/java/com/mmall/util/RedisShardedPoolUtil.java
+++ src/main/java/com/mmall/util/RedisShardedPoolUtil.java
@@ -71,6 +71,36 @@ public class RedisShardedPoolUtil {
         return result;
     }
 
+    public static String getSet(String key, String value) {
+        ShardedJedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.getSet(key, value);
+        } catch (Exception e) {
+            log.error("getSet key:{} value:{} error", key, value, e);
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long setNx(String key, String value) {
+        ShardedJedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.setnx(key, value);
+        } catch (Exception e) {
+            log.error("setnx key:{} value:{} error", key, value, e);
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
     public static Long del(String key) {
         ShardedJedis jedis = null;
         Long result = null;
